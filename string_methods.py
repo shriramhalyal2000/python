2c3ad171c6d04e1f5e505792cc6e2b2292e3eb78
Shriram Halyal <sshalyal42@gmail.com>
2024-10-21 20:07:32 +0530

    adding timeline file and strings file to the repo

---
+++ string_methods.py
@@ -0,0 +1,6 @@
+# # # string methods in built in python library 
+# # # len(variable), .upper(), . lower(), .capitalize(), .count(# ), .find(to count the first occurance of the given char)
+
+varible=input()
+result=count(varible)
+print(result)
