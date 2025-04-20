b46daa148ac9d3c7dcbbb94127abaee1f4792b0e
shriramhalyal2000 <shriramhalyal48@gmail.com>
2025-04-20 09:24:04 +0000

    code for calculator operations

--- TechWorldNana/basic_python/newProjects/calculator/main/main.py
+++ TechWorldNana/basic_python/newProjects/calculator/main/main.py
@@ -1,1 +1,17 @@
 # new calculator code for python knolwedge
+
+def addition(num1, num2):
+    result = num1 + num2
+    print(f" {result} is expected addition result")
+
+def substraction(num1, num2):
+    result=num1-num2
+    print(f"{result} is expect substraction")
+
+def multiplication(num1, num2):
+    result=num1*num2
+    print(f"{result} is expected multiplied number")
+
+def division(num1, num2):
+    result=num1/num2
+    print(f"{result} is ecpected division number")
