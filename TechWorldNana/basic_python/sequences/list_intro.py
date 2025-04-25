ab5531ab94c532a0e6435b3f979db15506f124b1
shriramhalyal2000 <shriramhalyal48@gmail.com>
2025-04-25 11:56:39 +0000

    adding list_intro

---
+++ TechWorldNana/basic_python/sequences/list_intro.py
@@ -0,0 +1,13 @@
+# new buchalka videos lists in python
+
+computer_parts=["Monitor", "CPU", "Keyboard", "Mouse", "Mousepad", "WebCam"]
+for part in computer_parts:
+    print(part)
+
+print()
+
+print(computer_parts[0:3])
+print(computer_parts[0:2])
+print(computer_parts[0:-1])
+print(computer_parts[-1])#printing the last part itself in the list
+print(computer_parts[-3])#printing the last but third in list
