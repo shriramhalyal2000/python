4023b2fd2388662c8ce46929d942ff7d398bcb65
shriramhalyal2000 <shriramhalyal48@gmail.com>
2025-04-27 16:46:51 +0000

    new commit

--- TechWorldNana/basic_python/sequences/list_intro.py
+++ TechWorldNana/basic_python/sequences/list_intro.py
@@ -1,5 +1,5 @@
 # new buchalka videos lists in python
-
+# lists are mutable
 computer_parts=["Monitor", "CPU", "Keyboard", "Mouse", "Mousepad", "WebCam"]
 for part in computer_parts:
     print(part)
@@ -10,5 +10,5 @@ print(computer_parts[0:2])
 print(computer_parts[0:-1])
 # printing the last part itself in the list
 print(computer_parts[-1])
-# printing the last but third in list 
+# printing the last but third in list
 print(computer_parts[-3])
