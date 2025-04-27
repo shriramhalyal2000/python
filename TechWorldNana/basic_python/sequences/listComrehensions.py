a98aa1d7241d6021bcb168f28406cec2fd9d6f53
shriramhalyal2000 <shriramhalyal48@gmail.com>
2025-04-27 18:06:04 +0000

    adding a new list code

---
+++ TechWorldNana/basic_python/sequences/listComrehensions.py
@@ -0,0 +1,15 @@
+# hackerrank list challenges
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+result =[]
+for i in range (x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            if (x+y+z) != n:
+                result.append([i,j,k])
+print(result)
+
