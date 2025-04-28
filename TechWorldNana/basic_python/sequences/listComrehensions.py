ad8748cfcf0bba68109fcf29c0a92294cc13993c
shriramhalyal2000 <shriramhalyal48@gmail.com>
2025-04-28 12:27:15 +0000

    adding real code (i+j+k)!=n

--- TechWorldNana/basic_python/sequences/listComrehensions.py
+++ TechWorldNana/basic_python/sequences/listComrehensions.py
@@ -5,11 +5,11 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
 
-result =[]
-for i in range (x+1):
+result = []
+for i in range(x+1):
     for j in range(y+1):
         for k in range(z+1):
-            if (x+y+z) != n:
+            if (i+j+k) != n:
                 result.append([i,j,k])
 print(result)
 
