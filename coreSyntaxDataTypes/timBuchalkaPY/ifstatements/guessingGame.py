cff49e7d5148495a8a1a8d6db1604166fcb27368
shriramhalyal2000 <shiramhalyal48@gmail.com>
2025-10-17 20:21:07 +0530

    adding some additional loop

--- coreSyntaxDataTypes/timBuchalkaPY/ifstatements/guessingGame.py
+++ coreSyntaxDataTypes/timBuchalkaPY/ifstatements/guessingGame.py
@@ -5,7 +5,17 @@ guess=int(input("please enter your guess between 1 and 10:\n"))
 
 if guess > answer:
   print("your guess is higher, please guess lower value.")
+  guess=int(input("Please try again:"))
+  if guess==answer:
+    print("You've guesed correctly")
+  else:
+    print("Better luck next time")
 elif guess < answer:
   print("your guess is lower, please guess higher.")
+  guess=int(input("enter your guess agaun:"))
+  if guess==answer:
+    print("You've finally got it")
+  else:
+    print("Better luck next time")
 else:
   print("you have guessed thr right answer!!!")
