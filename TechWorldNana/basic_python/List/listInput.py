f2ce3a3bee0650415ef3da981c75a94d0245cc33
shriramhalyal2000 <shiramhalyal48@gmail.com>
2025-10-20 17:30:38 +0530

    adding input list

---
+++ TechWorldNana/basic_python/List/listInput.py
@@ -0,0 +1,5 @@
+list_input=input("Enter the names list, seperated by comma \n")
+
+print(list_input)
+mysortedlist=list_input.split() #sort method prints the input in list format, seperated by commas
+print(mysortedlist)
