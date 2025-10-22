313289484b90ecffddfa95bba7296e0c43d58d2f
shriramhalyal2000 <shiramhalyal48@gmail.com>
2025-10-22 11:01:59 +0530

    adding untracked files

---
+++ coreSyntaxDataTypes/timBuchalkaPY/nestedloopsLIST/invertedPyrmd.py
@@ -0,0 +1,5 @@
+rows=5
+for i in range(rows, 0, -1):
+    space=rows-i
+    star=2*i-1
+    print(" "*space)
---
+++ coreSyntaxDataTypes/timBuchalkaPY/nestedloopsLIST/trianglespaced.py
@@ -0,0 +1,5 @@
+rows=5
+for i in range(1, rows+1):
+    star=2*i-1 #odd number stars
+    space= rows-i
+    print(" "* space+"*"*star)
